039d295e53b1be819ee01417a03c5d4daf9c5bad
tanabe@ocv171398 <tanabe.atsushi@gmail.com>
2020-07-20 14:39:59 +0900

    Add batch file to get and save disk usage into log file.
    commands arer extracted from main batch file.

---
+++ save-driveUsed.bat
@@ -0,0 +1,17 @@
+@echo off 
+
+rem このバッチファイルのディレクトリにバックアップを取る．
+cd /d %~dp0
+
+rem ログファイル名
+set LOGFILE=%~pd0\DriveUsed.log
+
+rem rsync.exeへのパス．通常はMSYSインストールディレクトリの usr\bin
+set RSYNCPATH=c:\msys64\usr\bin
+
+rem ディスク容量の保存
+echo ======================================================>> %LOGFILE%
+echo A snapshot was saved at %DATE% %TIME% >> %LOGFILE%
+fsutil volume diskFree %~d0 >> %LOGFILE%
+df  -hBM   .  >> %LOGFILE%
+
