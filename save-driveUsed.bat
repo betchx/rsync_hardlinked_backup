b7c6f8f3ef623b16febe1cbfe805b3ef5a42645e
tanabe@ocv171398 <tanabe.atsushi@gmail.com>
2020-08-07 09:28:10 +0900

    dfが動く様に修正
    PATHの設定が抜けていた．

--- save-driveUsed.bat
+++ save-driveUsed.bat
@@ -9,6 +9,9 @@ set LOGFILE=%~pd0\DriveUsed.log
 rem rsync.exeへのパス．通常はMSYSインストールディレクトリの usr\bin
 set RSYNCPATH=c:\msys64\usr\bin
 
+rem rsyncのパスを設定
+set PATH=%RSYNCPATH%;%PATH%
+
 rem ディスク容量の保存
 echo ======================================================>> %LOGFILE%
 echo A snapshot was saved at %DATE% %TIME% >> %LOGFILE%
